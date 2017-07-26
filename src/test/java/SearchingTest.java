d96003922955838385c649537a710ac7657f6673
Ali-X <alina240895@yandex.ru>
2017-07-26 21:01:01 +0300

    Add tests

--- src/test/java/SearchingTest.java
+++ src/test/java/SearchingTest.java
@@ -12,8 +12,44 @@ public class SearchingTest {
     }
 
     @org.junit.Test
-    public void shiftedSortedArray() {
+    public void shiftedSortedArray_1() {
+        int[] array = new int[]{3, 4, 5, 6, 9, 10, 15, 1};
+        assertTrue(search.binarySearch(array, find));
+    }
+
+    @org.junit.Test
+    public void shiftedSortedArray_2() {
+        int[] array = new int[]{4, 5, 6, 9, 10, 15, 1, 3};
+        assertTrue(search.binarySearch(array, find));
+    }
+
+    @org.junit.Test
+    public void shiftedSortedArray_3() {
+        int[] array = new int[]{5, 6, 9, 10, 15, 1, 3, 4};
+        assertTrue(search.binarySearch(array, find));
+    }
+
+    @org.junit.Test
+    public void shiftedSortedArray_4() {
+        int[] array = new int[]{6, 9, 10, 15, 1, 3, 4, 5};
+        assertTrue(search.binarySearch(array, find));
+    }
+
+    @org.junit.Test
+    public void shiftedSortedArray_5() {
+        int[] array = new int[]{9, 10, 15, 1, 3, 4, 5, 6};
+        assertTrue(search.binarySearch(array, find));
+    }
+
+    @org.junit.Test
+    public void shiftedSortedArray_6() {
         int[] array = new int[]{10, 15, 1, 3, 4, 5, 6, 9};
+        assertTrue(search.binarySearch(array, find));
+    }
+
+    @org.junit.Test
+    public void shiftedSortedArray_7() {
+        int[] array = new int[]{15, 1, 3, 4, 5, 6, 9, 10};
         assertTrue(search.binarySearch(array, find));
     }
 

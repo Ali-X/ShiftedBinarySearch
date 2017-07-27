303b1147a9245231f5efc9989a93190d2d883e33
Ali-X <alina240895@yandex.ru>
2017-07-27 13:30:15 +0300

    Code is completely changed

--- src/main/java/Searching.java
+++ src/main/java/Searching.java
@@ -1,24 +1,39 @@
 public class Searching {
 
     public boolean binarySearch(int[] data, int key) {
-        int size = data.length;
-        int low = 0;
-        int high = size - 1;
+        int length = data.length;
+        int shift = getShift(data, length);
+        return binarySearch(key, data, length, shift);
 
-        while (high >= low) {
-            int middle = (low + high) / 2;
-            if (data[middle] == key) {
+    }
+
+    private int getShift(int[] data, int length) {
+        int shift = 0;
+        while ((shift + 1 < length) && (data[shift] < data[shift + 1])) {
+            shift++;
+        }
+        if (shift == length - 1) {
+            return 0;
+        } else {
+            return shift + 1;
+        }
+    }
+
+
+    private boolean binarySearch(int key, int[] data, int length, int shift) {
+        int begin = 0;
+        int end = length - 1;
+
+        while (begin <= end) {
+            int mid = (end + begin) / 2;
+            int mid_shifted = (mid + shift) % length;
+
+            if (data[mid_shifted] == key) {
                 return true;
-            }
-            if(data[middle] >= data[high]) {
-                low = middle;
-                continue;
-            }
-            if (data[middle] < key) {
-                low = middle + 1;
-            }
-            if (data[middle] > key) {
-                high = middle - 1;
+            } else if (data[mid_shifted] < key) {
+                begin = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
         return false;
